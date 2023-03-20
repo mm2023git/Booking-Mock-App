be5753165a01d5872d31601c812d01223419e8ba
Mokel Moo <mokelmoo@pop-os.localdomain>
2023-03-20 12:52:59 -0400

    Setup fontawesome icon api. Made small changes to example divs

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,22 +2,35 @@ import hotel from './images/hotel.jpeg'
 import hotel1 from './images/hotel1.jpeg'
 import hotel2 from './images/hotel2.jpeg'
 import './Home.css'
+import './icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+
 
 function Home(){
     return(
         <div className="home">
+            
             <h1 className='motto'>Begin your next journey with us.</h1>
             <div className='deal-containers'>
                 
-                <div className="hotel-example1">
+                <div className="hotel-example">
                     <img src= {hotel} className = "hotelimg" alt='hotel'/>
+                    <div className='lower-box'>
+                        <div className='price-bar'>
+                            <FontAwesomeIcon icon = {faLocationDot} className="pinicon" />
+                            <p className='location'> San Juan</p>
+                            <p className='hotel-name'>Wyndham Resort</p>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className='hotel-example1'>
+                <div className='hotel-example'>
                     <img src= {hotel1} className = "hotelimg" alt='hotel'/>
                 </div>
 
-                <div className='hotel-example1'>
+                <div className='hotel-example'>
                     <img src= {hotel2} className = "hotelimg" alt='hotel'/>
                 </div>
 

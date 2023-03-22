3e77590208684c8288b33a246df744b6f382a569
mm2023git <maym202200@gmail.com>
2023-03-21 22:08:37 -0400

    Improve resolution scaling adding amenities grid to hotel cards

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import hotel1 from './images/hotel1.jpeg'
 import hotel2 from './images/hotel2.jpeg'
 import './Home.css'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import { faLocationDot, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot, faSearch, faWifi, faDumbbell, faDog, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import {Link} from "react-router-dom"
 
 function Home(){
@@ -25,14 +25,27 @@ function Home(){
                 <Link to ="./login" className='hotel-example'>
                     <img src= {hotel} className = "hotelimg" alt='hotel'/>
                     <div className='lower-box'>
-                        <div className='price-bar'>
+                        <div className='loc-bar'>
                             <FontAwesomeIcon icon = {faLocationDot} className="pinicon" />
                             <p className='location'> San Juan</p>
                             <p className='hotel-name'>Wyndham Resort</p>
                         </div>
-                        <div>
-                            <div className='amenities-row'>
-                                
+                        <div className='amenities-row'>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faWifi} className="amenicon" />
+                                <p className='icon-text'>Wi-fi</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDumbbell} className="amenicon" />
+                                <p className='icon-text'>Gym</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDog} className="amenicon" />
+                                <p className='icon-text'>Pet Friendly</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faCoffee} className="amenicon" />
+                                <p className='icon-text'>Continental Breakfast</p>
                             </div>
                         </div>
                     </div>
@@ -40,21 +53,58 @@ function Home(){
                 <Link to ="./login" className='hotel-example'>
                     <img src= {hotel1} className = "hotelimg" alt='hotel'/>
                     <div className='lower-box'>
-                        <div className='price-bar'>
+                        <div className='loc-bar'>
                             <FontAwesomeIcon icon = {faLocationDot} className="pinicon" />
                             <p className='location'>Shanghai</p>
                             <p className='hotel-name'>Shangri-La Resort</p>
                         </div>
+                        <div className='amenities-row'>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faWifi} className="amenicon" />
+                                <p className='icon-text'>Wi-fi</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDumbbell} className="amenicon" />
+                                <p className='icon-text'>Gym</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDog} className="amenicon" />
+                                <p className='icon-text'>Pet Friendly</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faCoffee} className="amenicon" />
+                                <p className='icon-text'>Continental Breakfast</p>
+                            </div>
+                        </div>
+                        
                     </div>
                 </Link>
 
                 <Link to ="./login" className='hotel-example'>
                     <img src= {hotel2} className = "hotelimg" alt='hotel'/>
                     <div className='lower-box'>
-                        <div className='price-bar'>
+                        <div className='loc-bar'>
                             <FontAwesomeIcon icon = {faLocationDot} className="pinicon" />
                             <p className='location'>Paris</p>
                             <p className='hotel-name'>Monsieur Helder</p>
+                        </div>
+                        <div className='amenities-row'>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faWifi} className="amenicon" />
+                                <p className='icon-text'>Wi-fi</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDumbbell} className="amenicon" />
+                                <p className='icon-text'>Gym</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faDog} className="amenicon" />
+                                <p className='icon-text'>Pet Friendly</p>
+                            </div>
+                            <div className='icon'>
+                                <FontAwesomeIcon icon = {faCoffee} className="amenicon" />
+                                <p className='icon-text'>Continental Breakfast</p>
+                            </div>
                         </div>
                     </div>
                 </Link>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,19 +2,18 @@ import './Navbar.css'
 import logo from './images/logo.png'
 import {Link} from "react-router-dom"
 
-
 function Navbar(){
 
 
     return(
         <div className = "topBar">
-            <div className="brand">
+            <Link to="../" className="brand">
                 <img src={logo} className="plane" alt='logo'></img>
-            </div>
+            </Link>
             <div className="nav-hold">
                 <Link className='nav-items'>Trips</Link>
                 <Link className='nav-items'>Support</Link>
-                <Link className='nav-items'>Login</Link>
+                <Link  to ="./login" className='nav-items'>Login</Link>
             </div>
             
         </div>
